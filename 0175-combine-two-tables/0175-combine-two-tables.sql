b5b7a8303610ae940b24180861bc78c0a7d6bc1d
CristianDelGobbo <125733919+cris1618@users.noreply.github.com>
2024-07-30 19:35:00 +0200

    Time: 384 ms (56.00%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,11 +1,14 @@
 # Write your MySQL query statement below
-SELECT p.firstName, p.lastName, 
-        CASE WHEN a.city IS NULL THEN null
-        ELSE a.city
-        END AS city, 
-        CASE WHEN a.state IS NULL THEN null
-        ELSE a.state
-        END AS state
-FROM Person AS p
-LEFT JOIN Address AS a ON p.personId = a.personId
+SELECT 
+    p.firstName, 
+    p.lastName, 
+    COALESCE(a.city, null) AS city, 
+    COALESCE(a.state, null) AS state
+FROM 
+    Person AS p
+LEFT JOIN 
+    Address AS a 
+ON 
+    p.personId = a.personId;
+
 
