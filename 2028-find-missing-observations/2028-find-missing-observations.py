1b3584dafe19ee28dc5d6fa645f8455eb181cfff
CristianDelGobbo <125733919+cris1618@users.noreply.github.com>
2024-09-05 14:16:43 +0200

    Time: 978 ms (67.91%), Space: 26.7 MB (74.60%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.py
@@ -0,0 +1,28 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m = len(rolls)
+        
+        sum_m = sum(rolls)
+        m_avg = sum_m/m
+        remaining_sum = mean*(n + m) - sum_m
+        
+        max_sum = n * 6
+        min_sum = n
+        
+        if remaining_sum > max_sum or remaining_sum < min_sum:
+            return []
+        
+        distribute_mean = int(remaining_sum/n)
+        mod = remaining_sum % n
+        
+        new_list = [distribute_mean] * n
+        for i in range(mod):
+            new_list[i] += 1
+        
+        return new_list
+            
+        
+        
+            
+            
+        
