eb45c897a8ddc626c8a3e21cec978c7f8197edad
CristianDelGobbo <125733919+cris1618@users.noreply.github.com>
2024-09-20 16:38:51 +0200

    Time: 72 ms (50.52%), Space: 17 MB (41.65%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        n = len(s)
+        reversed_string = s[::-1]
+        
+        for i in range(n):
+            if s[: n-i] == reversed_string[i:]:
+                return reversed_string[:i] + s
+        return ""
+        
