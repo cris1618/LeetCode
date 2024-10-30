c88b1fe99f874842fd9188f233bd82b0c27d702c
CristianDelGobbo <125733919+cris1618@users.noreply.github.com>
2024-10-30 14:13:15 +0100

    Time: 43 ms (69.77%), Space: 16.9 MB (13.17%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -1,27 +1,47 @@
 class Solution:
+    def getLongestIncreasingSubsequenceLength(self, v: List[int]) -> List[int]:
+        lis_len = [1] * len(v)
+        lis = [v[0]]
+        
+        for i in range(1, len(v)):
+            index = self.lowerBound(lis, v[i])
+            
+            if index == len(lis):
+                lis.append(v[i])
+            else:
+                lis[index] = v[i]
+            
+            lis_len[i] = len(lis)
+        
+        return lis_len
+    
+    def lowerBound(self, lis: List[int], target: int) -> int:
+        left, right = 0, len(lis)
+        while left < right:
+            mid = left + (right - left) // 2
+            if lis[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left
+    
     def minimumMountainRemovals(self, nums: List[int]) -> int:
         N = len(nums)
         
-        lis_length = [1] * N
-        lds_length = [1] * N
+        lis_length = self.getLongestIncreasingSubsequenceLength(nums)
         
-        # Stores the length of longest increasing subsequence that ends at i.
-        for i in range(N):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    lis_length[i] = max(lis_length[i], lis_length[j] + 1)
+        nums.reverse()
+        lds_length = self.getLongestIncreasingSubsequenceLength(nums)
         
-        # Stores the length of longest decreasing subsequence that starts at i
-        for i in range(N-1, -1, -1):
-            for j in range(i + 1, N):
-                if nums[i] > nums[j]:
-                    lds_length[i] = max(lds_length[i], lds_length[j] + 1)
+        lds_length.reverse()
         
         min_removals = float("inf")
         for i in range(N):
             if lis_length[i] > 1 and lds_length[i] > 1:
                 min_removals = min(
-                    min_removals, N-lis_length[i] - lds_length[i] + 1
+                    min_removals, N-lis_length[i]-lds_length[i] + 1
                 )
                 
         return min_removals
+       
