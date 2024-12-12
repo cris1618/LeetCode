e9267f5acf758a04c019fb4ad7345c7df5c16ed3
CristianDelGobbo <125733919+cris1618@users.noreply.github.com>
2024-12-12 09:04:05 +0100

    Time: 27 ms (32.92%), Space: 17.7 MB (8.46%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        result = 0
+        
+        for i in range(k):
+            gifts.sort(reverse=True)
+            gifts[0] = floor(gifts[0] ** (1/2))
+        
+        print(gifts)
+            
+        
+        for j in range(len(gifts)):
+            result += gifts[j]
+            
+        return result
+        
