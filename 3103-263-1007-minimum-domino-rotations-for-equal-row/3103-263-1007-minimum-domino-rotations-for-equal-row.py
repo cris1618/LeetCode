915a7a606fa7c6abe4c225d9bdbfe78751c2913b
CristianDelGobbo <125733919+cris1618@users.noreply.github.com>
2025-05-03 15:05:00 +0200

    Time: 30 ms, Memory: 18.5 MB - LeetHub

---
+++ 3103-263-1007-minimum-domino-rotations-for-equal-row/3103-263-1007-minimum-domino-rotations-for-equal-row.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        def check(x):
+            rotations_top = rotations_bottom = 0
+            for i in range(len(tops)):
+                if tops[i] != x and bottoms[i] != x:
+                    return float("inf") 
+                elif tops[i] != x:
+                    rotations_top += 1
+                elif bottoms[i] != x:
+                    rotations_bottom += 1
+            return min(rotations_top, rotations_bottom)
+
+        candidates = [tops[0], bottoms[0]]
+        min_rotations = min(check(candidates[0]), check(candidates[1]))
+
+        return min_rotations if min_rotations != float("inf") else -1
+
+            
+                
+
+
+        
+        
